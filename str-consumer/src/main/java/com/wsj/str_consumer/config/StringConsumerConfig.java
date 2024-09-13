03c8119a0523e141a57bd6da2924ba6d3eab2fb3
william sarti <williamsartijose@hotmail.com>
2024-09-13 11:23:23 +0100

    [str-consumer] Iniciando String Consumer

--- str-consumer/src/main/java/com/wsj/str_consumer/config/StringConsumerConfig.java
+++ str-consumer/src/main/java/com/wsj/str_consumer/config/StringConsumerConfig.java
@@ -19,42 +19,74 @@ import java.util.HashMap;
 @Configuration
 public class StringConsumerConfig {
 
+    // Injeta automaticamente as propriedades de configuração do Kafka definidas no arquivo application.yml
     private final KafkaProperties properties;
 
+    // Configuração do factory do consumidor Kafka
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
+        // Cria um mapa de configurações para o consumidor Kafka
         var configs = new HashMap<String, Object>();
+
+        // Define o endereço do Kafka Broker
         configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, properties.getBootstrapServers());
+
+        // Configura o deserializador de chave (key) como String
         configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        // Configura o deserializador de valor (value) como String
         configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        // Retorna uma fábrica de consumidores com as configurações especificadas
         return new DefaultKafkaConsumerFactory<>(configs);
     }
 
+    // Cria a configuração para o container que irá escutar mensagens do Kafka
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, String> strContainerFactory(
             ConsumerFactory<String, String> consumerFactory
     ) {
+        // Cria uma nova fábrica de listeners para consumir as mensagens do Kafka
         var factory = new ConcurrentKafkaListenerContainerFactory<String, String>();
+
+        // Associa a fábrica de consumidores criada anteriormente
         factory.setConsumerFactory(consumerFactory);
+
+        // Retorna a fábrica de listeners configurada
         return factory;
     }
 
+    // Configura um container com interceptação de mensagens válidas
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, String> validMessageContainerFactory(
             ConsumerFactory<String, String> consumerFactory
     ) {
+        // Cria uma nova fábrica de listeners com interceptação de mensagens
         var factory = new ConcurrentKafkaListenerContainerFactory<String, String>();
+
+        // Associa a fábrica de consumidores padrão
         factory.setConsumerFactory(consumerFactory);
+
+        // Define um interceptor para validar as mensagens recebidas
         factory.setRecordInterceptor(validMessage());
+
+        // Retorna a fábrica de listeners configurada com interceptor
         return factory;
     }
 
+    // Intercepta e valida as mensagens consumidas do Kafka
     private RecordInterceptor<String, String> validMessage() {
         return record -> {
+            // Verifica se a mensagem contém a palavra "Teste"
             if(record.value().contains("Teste")) {
-                log.info("Posssui a palavra Teste");
+                // Loga uma mensagem se "Teste" estiver presente
+                log.info("Possui a palavra Teste");
+
+                // Retorna o registro para continuar o processamento
                 return record;
             }
+
+            // Se a mensagem não contiver "Teste", simplesmente retorna o registro
             return record;
         };
     }
