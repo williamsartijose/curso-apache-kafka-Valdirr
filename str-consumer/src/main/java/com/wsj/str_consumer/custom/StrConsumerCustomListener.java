3e7e83d8b4e7a67ba37511381c02ec6959d5c68e
william sarti <williamsartijose@hotmail.com>
2024-09-13 15:16:47 +0100

    [str-consumer] Implementando Listener Customizado

--- str-consumer/src/main/java/com/wsj/str_consumer/custom/StrConsumerCustomListener.java
+++ str-consumer/src/main/java/com/wsj/str_consumer/custom/StrConsumerCustomListener.java
@@ -1,6 +1,5 @@
 package com.wsj.str_consumer.custom;
 
-
 import org.springframework.core.annotation.AliasFor;
 import org.springframework.kafka.annotation.KafkaListener;
 
@@ -9,21 +8,34 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+// Define a retenção da anotação em tempo de execução (Runtime)
+// Isso garante que a anotação estará disponível para ser lida em tempo de execução pela JVM
 @Retention(RetentionPolicy.RUNTIME)
+
+// Define o alvo da anotação, que neste caso são métodos
 @Target(ElementType.METHOD)
+
+// Anotação customizada que usa a anotação KafkaListener como base
 @KafkaListener
 public @interface StrConsumerCustomListener {
 
+    // Mapeia o atributo "topics" da anotação KafkaListener para nossa anotação customizada
+    // O valor padrão é "str-topic", ou seja, o tópico que será consumido por padrão
     @AliasFor(annotation = KafkaListener.class, attribute = "topics")
     String[] topics() default "str-topic";
 
+    // Mapeia o atributo "containerFactory" da anotação KafkaListener para nossa anotação customizada
+    // Define o containerFactory que será usado para consumir as mensagens. O valor padrão é "strContainerFactory"
     @AliasFor(annotation = KafkaListener.class, attribute = "containerFactory")
     String containerFactory() default "strContainerFactory";
 
+    // Mapeia o atributo "groupId" da anotação KafkaListener para nossa anotação customizada
+    // Define o grupo de consumidores (groupId) que irá consumir as mensagens
     @AliasFor(annotation = KafkaListener.class, attribute = "groupId")
     String groupId() default "";
 
+    // Mapeia o atributo "errorHandler" da anotação KafkaListener para nossa anotação customizada
+    // Define o manipulador de erros a ser usado em caso de falha no consumo das mensagens
     @AliasFor(annotation = KafkaListener.class, attribute = "errorHandler")
     String errorHandler() default "errorCustomHandler";
-
 }
--- str-consumer/src/main/java/com/wsj/str_consumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/wsj/str_consumer/listeners/StrConsumerListener.java
@@ -10,20 +10,31 @@ import org.springframework.stereotype.Component;
 @Component
 public class StrConsumerListener {
 
+    // Método que consome mensagens do Kafka usando a anotação customizada
+    // O grupo de consumidores usado aqui é "group-1"
     @SneakyThrows
     @StrConsumerCustomListener(groupId = "group-1")
     public void create(String message) {
+        // Loga a mensagem recebida no tópico Kafka
         log.info("CREATE ::: Receive message {}", message);
+
+        // Força uma exceção para simular um erro no processamento da mensagem
         throw new IllegalArgumentException("EXCEPTION...");
     }
 
+    // Outro método que consome mensagens do Kafka, também com o "group-1"
+    // Usando a anotação customizada, ele consome mensagens do tópico padrão definido
     @StrConsumerCustomListener(groupId = "group-1")
     public void log(String message) {
+        // Loga a mensagem recebida
         log.info("LOG ::: Receive message {}", message);
     }
 
+    // Método que consome mensagens usando a anotação padrão KafkaListener
+    // O grupo de consumidores é "group-2" e o containerFactory é o validMessageContainerFactory
     @KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "validMessageContainerFactory")
     public void history(String message) {
+        // Loga a mensagem recebida
         log.info("HISTORY ::: Receive message {}", message);
     }
 }
